b74e748d22a77738b86d814cf70ea7fb68c24759
gabrielaliera <gliera2990@gmail.com>
2023-07-15 15:29:28 -0700

    Added db code, api endpoint routes

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,32 @@
 import os
 from flask import Flask, render_template, request
 from dotenv import load_dotenv
+from peewee import *
+import datetime 
+from playhouse.shortcuts import model_to_dict 
 
 load_dotenv()
 app = Flask(__name__)
 
-#/work / experience / education / places we visited
-# updated flask routes
+mydb = MySQLDatabase(os.getenv("MYSQL_DATABASE"),
+              user=os.getenv("MYSQL_USER"),
+              password=os.getenv("MYSQL_PASSWORD"),
+              host=os.getenv("MYSQL_HOST"),
+              port=3306
+)
+print(mydb)
+
+class TimelinePost(Model):
+    name = CharField()
+    email = CharField()
+    content = TextField()
+    created_at = DateTimeField(default=datetime.datetime.now)
+
+    class Meta:
+        database = mydb
+
+mydb.connect()
+mydb.create_tables([TimelinePost])
 
 @app.route('/')
 def index():
@@ -72,3 +92,35 @@ def Hobbies():
 @app.route('/Locations')  # Define the route for /Map
 def Map():
     return render_template('Locations.html', title="Location", url=os.getenv("URL"))
+
+@app.route('/api/timeline_post', methods=['POST'])
+def post_time_line_post():
+    name = request.form['name']
+    email = request.form['email']
+    content = request.form['content']
+    timeline_post =TimelinePost.create(name=name, email=email, content=content)
+
+    return model_to_dict(timeline_post)
+
+@app.route('/api/timeline_post', methods=['GET'])
+def get_time_line_post():
+    return {
+        'timeline_posts' : [
+            model_to_dict(p)
+            for p in TimelinePost.select().order_by(TimelinePost.created_at.desc())
+        ]
+    }
+
+@app.route('/api/timeline_post', methods=["DELETE"])
+def delete_time_line_post(post_id):
+    post_id = request.args.get('post_id')
+    try:
+        post = TimelinePost.get_by_id(post_id)
+        post.delete_instance()
+        return "Timeline post deleted successfully"
+    except TimelinePost.DoesNotExist:
+        return "Timeline post not found", 404
+    
+@app.route('/timeline')
+def timeline():
+    return render_template('timeline.html', title="Timeline", url=os.getenv("URL"))
