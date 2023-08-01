3f8c3222d3b3b289ce875196d3cf0ff3bbb4fdf2
gabrielaliera <gliera2990@gmail.com>
2023-08-01 11:07:33 -0700

    Move data outside of init.py file

---
+++ app/data.py
@@ -0,0 +1,42 @@
+workData = [
+    {
+        "title": "Production Engineering Intern",
+        "employer": "MHL Fellowship",
+        "date": "June 2023 - Present",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+    },
+    {   
+        "title": "Software Engineering Intern",
+        "employer": "Meta",
+        "date": "June 2022 - September 2022", 
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+    },
+    {   
+        "title": "Participant",
+        "employer": "MHL Hackaton",
+        "date": "June 2022",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+    },
+    {
+        "title": "President",
+        "employer": "The Club",
+        "date": "Oct 2021 - May 2022", 
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+    }
+]
+
+# Hobbies data
+hobbyData = [
+    {"imgSource": "/static/img/podcast.jpg",
+    "name": "True Crime Podcasts", 
+    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit."},
+    {"imgSource": "/static/img/soccer.jpg", 
+    "name": "Soccer", 
+    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit."},
+    {"imgSource": "/static/img/travel.jpg", 
+    "name": "Traveling", 
+    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit."},
+    {"imgSource": "/static/img/garden.jpg", 
+    "name": "Gardening", 
+    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit."}
+]
