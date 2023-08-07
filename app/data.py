05aa3927a62f89c336ad2ed2eae4bcdd7740d194
gabrielaliera <gliera2990@gmail.com>
2023-08-07 16:59:22 -0700

    Add jobs and education data

--- app/data.py
+++ app/data.py
@@ -2,26 +2,72 @@ workData = [
     {
         "title": "Production Engineering Intern",
         "employer": "MHL Fellowship",
-        "date": "June 2023 - Present",
-        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+        "date": "Jun 2023 - Present",
+        "description": [
+            "Developing Flask website using Git & GitHub workflow to implementing new features such as Jinja templates, Bootstrap CSS & MySQL",
+            "Established a streamlined CI/CD pipeline for deploying the website on DigitalOcean droplet, leveraging Bash scripting & Docker containers for automated updates",
+            "Implementing test-driven development (TDD) approach to conduct unit testing for MySQL & integration testing for API endpoints",
+            "Collaborating with 10 others in agile scrum activities such as daily standups, retrospectives, & peer code reviews"                
+        ]
     },
     {   
-        "title": "Software Engineering Intern",
-        "employer": "Meta",
-        "date": "June 2022 - September 2022", 
-        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+        "title": "Tech Fellow - Web Development & Technical Interview Prep",
+        "employer": "CodePath",
+        "date": "Jan 2023 - Present", 
+        "description": [
+            "Facilitating active participation & engagement among 50 students by overseeing React, JavaScript, RESTful APIs, & data structures projects",
+            "Provide clear explanations of technical concepts and engage in discussions about potential solutions with students"
+        ]
     },
     {   
-        "title": "Participant",
-        "employer": "MHL Hackaton",
-        "date": "June 2022",
-        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+        "title": "Web Accessibility Technical Support II",
+        "employer": "Mt San Antonio College",
+        "date": "Oct 2021 - Present",
+        "description": [
+            "Improving web accessibility by identifying & troubleshooting errors for 5,800 documents by reformatting HTML using Adobe Acrobat Pro DC and SiteImprove",
+            "Expanded accessibility knowledge & access for 94 employees via outreach increasing help requests by 88%",
+            "Collaborating with college-wide accreditation team to make evidence accessible, reducing project costs by 23%"
+        ]
     },
     {
-        "title": "President",
-        "employer": "The Club",
-        "date": "Oct 2021 - May 2022", 
-        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+        "title": "Data Analysis Intern",
+        "employer": "City of Los Angeles , Information Technology Agency - Help Desk",
+        "date": "Feb 2022 - Jun 2022", 
+        "description": [
+            " Analyzed & optimize ServiceNow ticketing help-desk operations by utilizing SQL queries for data analysis reports",
+            "Transformed 63 queries into 35 data visualizations resulting in dashboard to monitor daily operations"
+        ]
+    },
+    {
+        "title": "Independent Travel",
+        "employer": "Australia, Philippines, Vietnam, Malaysia, Cambodia, Singapore, Indonesia, Thailand, China",
+        "date": "Aug 2016 - Feb 2020", 
+        "description": []
+    },
+     {
+        "title": "International Student Program Coordinator",
+        "employer": "University of Redlands",
+        "date": "Aug 2013 - Jun 2016", 
+        "description": [
+            "Managed orientation & supervised 12 peer-mentors for over 300 incoming students to ensure safe transition to United States",
+            "Increased involvement by organizing cultural events & support services leading to increase satisfaction by 14%"
+        ]
+    }
+]
+
+eduData = [
+    {
+        "degree": "A.S in Computer Programming",
+        "location":"Mt San Antonio College",
+        "year":"December 2022",
+        "img": "/static/img/mtsac.jpg"
+
+    },
+    {
+        "degree": "B.A in Anthropology",
+        "location":"University of California - San Diego",
+        "year":"June 2013",
+        "img": "/static/img/ucsd.png"
     }
 ]
 
